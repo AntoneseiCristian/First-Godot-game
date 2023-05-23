cbb2418171eb8dfed317af5803a1d117092e2219
Antonesei Cristian <antoneseicristian96@gmail.com>
2023-05-23 18:14:40 +0300

    v0.0
    door close/open

--- Scripts/Door1.gd
+++ Scripts/Door1.gd
@@ -2,6 +2,21 @@ extends Interactable
 @onready var animation_player = $AnimationPlayer
 
 var isOpen := false
+var canInteract := true
 
 func action_use():
-	pass
+	if isOpen:
+		close()
+	else:
+		open()
+
+func close():
+	animation_player.play("door_closed")
+	isOpen = false
+	
+	
+func open():
+	animation_player.play("door_open")
+	isOpen = true
+	
+	
