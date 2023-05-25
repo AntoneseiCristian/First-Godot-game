9fb7d44bc9b8650ed115976ed6da3e505a9a7728
Antonesei Cristian <antoneseicristian96@gmail.com>
2023-05-25 15:49:59 +0200

    v0.0

--- Scripts/Door1.gd
+++ Scripts/Door1.gd
@@ -5,18 +5,25 @@ var isOpen := false
 var canInteract := true
 
 func action_use():
-	if isOpen:
-		close()
-	else:
-		open()
+	if canInteract == true:
+		if isOpen:
+			close()
+		else:
+			open()
 
 func close():
 	animation_player.play("door_closed")
 	isOpen = false
+	canInteract = false
 	
 	
 func open():
 	animation_player.play("door_open")
 	isOpen = true
+	canInteract = false
 	
 	
+
+
+func _on_animation_player_animation_finished(anim_name):
+	canInteract = true
